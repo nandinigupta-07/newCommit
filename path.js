10bcea99f2d7b8c7608e673cbf5affd3f7d6f829
Nandini <guptanandini072@gmail.com>
2026-02-04 11:46:58 +0530

    path

---
+++ path.js
@@ -0,0 +1,5 @@
+console.log("Node.js is running 🚀");
+const path=require('path');
+const filePath = 'C:/Users/HP/Desktop/class/index.js';
+const dirName=path.dirname(filePath);
+console.log(dirName);
