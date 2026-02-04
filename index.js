1bf07ab4ad714a3d1c4116b5ab3ee0d89b82094b
Nandini <guptanandini072@gmail.com>
2026-02-04 12:37:03 +0530

    Fix os module usage and print home directory

--- index.js
+++ index.js
@@ -6,10 +6,5 @@ fs.writeFileSync('example.txt',"this is an example file created using Node.js",
         if(e) consloe.log("ye aaya error");
         consloe.log("done");
 });
-<<<<<<< HEAD
 console.log("file created successfully");
 console.log("file system operation completeed");
-=======
-consloe.log("file created successfully");
-consloe.log("file system operation completeed");
->>>>>>> 4084a7290766cb4f2256a00285cd596c0c3ab390
---
+++ os.js
@@ -0,0 +1,5 @@
+const os = require('os');
+
+const homeDirectory = os.homedir();
+
+console.log("Home directory:", homeDirectory);
